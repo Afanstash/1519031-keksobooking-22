8b38a26bc15b1095824ccaca1ea18d20b1971c6e
Anastasiia Ushakova <anastasiia.b.ushakova@gmail.com>
2021-02-10 17:45:56 +0700

    Рефакторинг. Смена типа данных

--- js/main.js
+++ js/main.js
@@ -4,22 +4,19 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/parseInt
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/pow
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/min
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
 
 function getRandomNumber(countMin, countMax, n = 0) {
-  let minNumber = Math.min(countMin, countMax);
-  let maxNumber = Math.max(countMin, countMax);
-  let mathRandomNumber = Math.random();
-  let randomNumber = parseInt((mathRandomNumber * (maxNumber - minNumber + 1) + minNumber) * Math.pow(10, n)) / Math.pow(10, n);
+  const minNumber = Math.min(countMin, countMax);
+  const maxNumber = Math.max(countMin, countMax);
+  const randomNumber = (Math.random() * (maxNumber - minNumber) + minNumber).toFixed(n);
+  // const randomNumber = parseInt((Math.random() * (maxNumber - minNumber) + minNumber) * Math.pow(10, n)) / Math.pow(10, n);
 
   if (minNumber < 0 || maxNumber < 0) {
-    return 'Введите положительные числа!';
+    return NaN;
   }
   if (minNumber === maxNumber) {
     return minNumber;
-  }
-  while (randomNumber < minNumber || randomNumber > maxNumber) {
-    mathRandomNumber = Math.random();
-    randomNumber = parseInt((mathRandomNumber * (maxNumber - minNumber + 1) + minNumber) * Math.pow(10, n)) / Math.pow(10, n);
   }
   return randomNumber;
 }
